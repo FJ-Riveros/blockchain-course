581e04064c9b9ae95ca2f3597b6e78c56c2c3866
FJ-Riveros <fran.etnies@gmail.com>
2023-02-09 00:26:23 +0100

    feat: creating the dynamic svg contract

---
+++ nfts-hardhat/contracts/DynamicSvgNft.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "base64-sol/base64.sol";
+
+contract DynamicSvgNft is ERC721 {
+    uint256 private s_tokenCounter;
+    string private i_lowImageURI;
+    string private i_highImageURI;
+    string private constant base64EncodedSvgPrefix = "data:iamge/svg+xml;base64,";
+
+    constructor(string memory lowSvg, string memory highSvg) ERC721("Dynamic SVG NFT", "DSN") {
+        s_tokenCounter = 0;
+    }
+
+    function svgToImageURI(string memory svg) public pure returns (string memory) {}
+
+    function mintNft() public {
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
+    }
+}
